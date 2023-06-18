576fae3f80308d63a0ceccb5b59817fe63d72eb5
Michael-okojie <michaelokojie1@gmail.com>
2023-06-18 15:01:13 +0100

    quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Printing a Quote for the advanced tasks
+ *
+ * Return: Always 1 (Success)
+*/
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
