2a3b82a0ca7ee048fab7d69637d3d1d06e7b67c2
Michael-okojie <michaelokojie1@gmail.com>
2023-06-19 00:55:36 +0100

    102-print_comb5 test1

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Printing combination of two double digit numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i; j <= 99; j++)
+		{
+			if (i != j)
+			{
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
+				putchar(' ');
+			}
+
+			if (j != i)
+			{
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
+				putchar(',');
+				putchar(' ');
+			}
+			/*if (i != 99 || j != 99)
+			{
+				
+			}*/
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
