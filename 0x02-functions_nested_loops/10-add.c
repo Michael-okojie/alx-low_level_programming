be436c12ee3d322abedb024ee220e1196109c26a
Michael-okojie <michaelokojie1@gmail.com>
2023-06-21 04:05:34 +0100

    function to add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adding two numbers
+ * @n: first number
+ * @c: second number
+ * Return: returns nine times table starting with zero
+ */
+
+int add(int n, int c)
+{
+	return (n + c);
+}
