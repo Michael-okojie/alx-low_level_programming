e1ca30234599f19f54d0e66d86cc64b63ad67293
Michael-okojie <michaelokojie1@gmail.com>
2023-06-22 20:23:45 +0100

    fizz buzz correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,21 +17,27 @@ int main(void)
 
 		if (fizz == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 
 		}
 		else if (buzz == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (buzz == 0 && fizz == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
 		}
 	}
+	printf("\n");
+
 	return (0);
 }
