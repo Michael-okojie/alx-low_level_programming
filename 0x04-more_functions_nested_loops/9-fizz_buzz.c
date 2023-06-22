e22c24999b5804dca61f8f9c79b2284ae434af74
Michael-okojie <michaelokojie1@gmail.com>
2023-06-22 20:11:30 +0100

    the fizz buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Fizzbuz test
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		int fizz = i % 3;
+		int buzz = i % 5;
+
+		if (fizz == 0 )
+		{
+			printf("Fizz ");
+
+		}
+		else if (buzz == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (buzz == 0 && fizz == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
