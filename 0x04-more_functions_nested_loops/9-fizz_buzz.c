66642bd93a97e0dc92cf40bf0f616caf9f7026d5
Michael-okojie <michaelokojie1@gmail.com>
2023-06-22 23:13:31 +0100

    finally fixed fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,23 +17,23 @@ int main(void)
 
 		if (fizz == 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		}
 		else if (buzz == 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		}
 		else if (buzz == 0 && fizz == 0)
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		}
-		else
+		else if (i == 1)
 		{
 			printf("%d", i);
 		}
-		if (i != 100)
+		else
 		{
-			printf(" ");
+			printf(" %d", i);
 		}
 	}
 	printf("\n");
