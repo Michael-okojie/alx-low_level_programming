5f8ef965ef51cbf2936f593b242e7e921b34f5c1
Michael-okojie <michaelokojie1@gmail.com>
2023-06-29 21:15:35 +0100

    concatenates two strings with at most n bytes from src

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,13 +12,10 @@ char *_strncat(char *dest, char *src, int n)
 	int dest_len = 0, index = 0;
 
 	while (dest[index++])
-	{
 		dest_len++;
-	}
 
 	for (index = 0; src[index] && index < n; index++)
-	{
 		dest[dest_len] = src[index];
-	}
+
 	return (dest);
 }
