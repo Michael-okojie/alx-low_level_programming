563451f64d3cb9552fc579d7d267b83eea42de1b
Michael-okojie <michaelokojie1@gmail.com>
2023-06-29 22:41:17 +0100

    letters to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,15 +8,15 @@
  */
 char *string_toupper(char *str)
 {
-	int i = 0;
+	char *ptr = str;
 
-	while (str[i])
+	while (*ptr != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (*ptr >= 'a' && *ptr <= 'z')
 		{
-			str[i] -= 32;
-			i++;
+			*ptr = *ptr - 32;
 		}
+		ptr++;
 	}
 	return (str);
 }
