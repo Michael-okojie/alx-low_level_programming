a0142b299c3577cd6ce995cec3852a5dc8f1b775
Michael-okojie <michaelokojie1@gmail.com>
2023-07-07 00:39:45 +0100

    check if a number is prime or not

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * prime_a - this was voundl
+ * @a: input
+ * @b: input
+ * Return: 0
+ */
+int prime_a(int a, int b)
+{
+	if (a <= 1 || (a != b && a % b == 0))
+	{
+		return (0);
+	}
+	else if (a == b)
+	{
+		return (1);
+	}
+	return (prime_a(a, b + 1));
+}
+
+/**
+ * is_prime_number - recursive function to check if a number is a prime number or not
+ * @n: number to be checked
+ * Return: if its prim eor not
+ */
+int is_prime_number(int n)
+{
+	return (prime_a(n, 2));
+}
