49f11d53b9ef45e4dbee5edd7f96170a162547a5
Michael-okojie <michaelokojie1@gmail.com>
2023-07-08 01:45:13 +0100

    program that prints the number of arguments passed into it

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,6 +2,7 @@
 /**
  * main -  program that prints the number of arguments passed into it
  * @argc: is an argument counter for char
+ * @argv: an array of arguements
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
