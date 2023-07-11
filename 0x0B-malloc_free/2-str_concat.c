aef5c1e35c512a8febcb9df903d05526823f88d4
Michael-okojie <michaelokojie1@gmail.com>
2023-07-11 20:38:26 +0100

    function to concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,15 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, length1, length2;
 	char *concatenated;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+    if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	while (s1[length1] != '\0')
 	{
 		length1++;
@@ -19,7 +28,6 @@ char *str_concat(char *s1, char *s2)
 
 	while (s2[length2] != '\0')
 	{
-		/* code */
 		length2++;
 	}
 
