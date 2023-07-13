524fcb0a51aa1f94ed29a65749e2c5eae98fab7b
Michael-okojie <michaelokojie1@gmail.com>
2023-07-14 00:53:08 +0100

    creating an array of integers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * array_range - creates an arrray of integers
+ * @min: minimum integer
+ * @max: maximum integer
+ *
+ * Return: pointer to the newly created array.
+ */
+int *array_range(int min, int max)
+{
+	int *array, size, i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	int size = max - min + 1;
+
+	array = malloc(size * sizeof(int));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = min;
+		min++;
+	}
+
+	return (array);
+}
